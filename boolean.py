53ee56b42e5fa0a2f54fc9be3d59595383077682
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:45:02 +0100

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,27 @@
+
+print(2009 < 6789)
+print(23 == 19)
+print(15 > 13)
+
+# evaluate value to check if it's true or false using IF and ELSE 
+laliga = 999
+premier_league = 597 
+# print(laliga > premier_league)
+# print (laliga < premier_league)
+
+if premier_league > laliga: 
+    print('premier_league atmosphere is better than laliga atmosphere')
+else : 
+    print('laliga atmosphere is better than premeir league atmosphere') 
+    
+if premier_league < laliga :
+    print('laliga players are skillful than premier league players')
+else :
+    print('premeir league players are skillful than laliga players')
+    
+print(bool (premier_league))
+
+print(bool (laliga))
+
+ 
+
